eaf27d2cccb9aae648303b413e81a52ffe006e02
mynameistj <32323599+mynameistj@users.noreply.github.com>
2019-11-27 23:20:55 +0700

    SqlLite

---
+++ app/src/main/java/bersatu/kita/part10/WordRepository.kt
@@ -0,0 +1,10 @@
+package bersatu.kita.part10
+
+import androidx.lifecycle.LiveData
+
+class WordRepository(private val wordDao : WordDao) {
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
